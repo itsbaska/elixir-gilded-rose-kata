252a3607afbeb3ef0c83afe2d4c746ac917a1457
Baska Batbold <baskabatbold@gmail.com>
2018-05-17 15:13:47 -0500

    update cuantity test

--- test/gilded_rose_test.exs
+++ test/gilded_rose_test.exs
@@ -1,6 +1,7 @@
 defmodule GildedRoseTest do
   use ExUnit.Case
 
-  test "begin the journey of refactoring" do
+  test "updates an item sell-in date" do
+    assert GildedRose.update_quality([%Item{name: "bread", sell_in: 9, quality: 2}]) == [%Item{name: "bread", sell_in: 8, quality: 1}]
   end
 end
