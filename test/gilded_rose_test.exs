8b9ed950fefc3e2061fc3b142b5e08252ca1a699
Baska Batbold <baskabatbold@gmail.com>
2018-05-17 15:23:32 -0500

    tests for backstage passes

--- test/gilded_rose_test.exs
+++ test/gilded_rose_test.exs
@@ -1,7 +1,24 @@
 defmodule GildedRoseTest do
   use ExUnit.Case
 
-  test "updates an item sell-in date" do
+  test "updates an item" do
     assert GildedRose.update_quality([%Item{name: "bread", sell_in: 9, quality: 2}]) == [%Item{name: "bread", sell_in: 8, quality: 1}]
   end
+
+  test "update an Aged Brie" do 
+    assert GildedRose.update_quality([%Item{name: "Aged Brie", sell_in: 9, quality: 2}]) == [%Item{name: "Aged Brie", sell_in: 8, quality: 3}]
+  end
+
+  test "update Backstage passes to a TAFKAL80ETC concert -- sell in < 10 > 5" do 
+    assert GildedRose.update_quality([%Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 9, quality: 2}]) == [%Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 8, quality: 4}]
+  end
+
+  test "update Backstage passes to a TAFKAL80ETC concert -- sell in < 5" do 
+    assert GildedRose.update_quality([%Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 5, quality: 2}]) == [%Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 4, quality: 5}]
+  end
+
+  test "update Backstage passes to a TAFKAL80ETC concert -- sell in < 0" do 
+    assert GildedRose.update_quality([%Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 0, quality: 3}]) == [%Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: -1, quality: 0}]
+  end
+
 end
