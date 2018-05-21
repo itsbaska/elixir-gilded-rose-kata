a218295e070369997f25c424bc937010f070a34d
Baska Batbold <baskabatbold@gmail.com>
2018-05-21 12:32:09 -0500

    remove comments

--- lib/item.ex
+++ lib/item.ex
@@ -26,39 +26,4 @@ defimpl GildedRose.Update, for: Item do
     end
     
   end
-  # def update_item(%Item{name: "Sulfuras, Hand of Ragnaros", sell_in: 0, quality: 0}) do
-  #   %Item{name: "Sulfuras, Hand of Ragnaros", sell_in: 0, quality: 0}
-  # end
-
-  # def update_item(%Item{name: "Aged Brie", quality: 50, sell_in: day}) do
-  #   %Item{name: "Aged Brie", sell_in: day - 1, quality: 50}
-  # end
-  
-  # def update_item(%Item{name: "Aged Brie", sell_in: day, quality: quality}) do
-  #   %Item{name: "Aged Brie", sell_in: day - 1, quality: quality + 1}
-  # end
-
-  # def update_item(%Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: day, quality: quality}) when day < 10 and day > 5 do
-  #   %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: day - 1, quality: quality + 2}
-  # end
-  
-  # def update_item(%Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: day, quality: quality}) when day <= 5 and day > 1 do
-  #   %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: day - 1, quality: quality + 3}
-  # end
-  
-  # def update_item(%Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: day, quality: _}) when day <= 0 do
-  #   %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: day - 1, quality: 0}
-  # end
-
-  # def update_item(%Item{name: name, sell_in: day, quality: 0}) when name != "Aged Brie" do
-  #   %Item{name: name, sell_in: day - 1, quality: 0}
-  # end
-
-  # def update_item(%Item{name: name, sell_in: day, quality: quality}) when name != "Aged Brie" and day < 0 do
-  #   %Item{name: name, sell_in: day - 1, quality: quality - 2}
-  # end
-
-  # def update_item(%Item{name: name, sell_in: day, quality: quality}) when name != "Aged Brie" do
-  #   %Item{name: name, sell_in: day - 1, quality: quality - 1}
-  # end
 end
--- lib/items/brie.ex
+++ lib/items/brie.ex
@@ -12,13 +12,4 @@ defimpl GildedRose.Update, for: Item.Brie do
     %{ item | quality: item.quality + 1 , sell_in: item.sell_in - 1}
   end
 
-    # def update_item(%Item{name: "Aged Brie", quality: 50, sell_in: day}) do
-  #   %Item{name: "Aged Brie", sell_in: day - 1, quality: 50}
-  # end
-  
-  # def update_item(%Item{name: "Aged Brie", sell_in: day, quality: quality}) do
-  #   %Item{name: "Aged Brie", sell_in: day - 1, quality: quality + 1}
-  # end
-
-    
 end
